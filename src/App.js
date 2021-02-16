d93f90daf74adf196d659e2d0c0728a49dc6b98f
eminvergil <emintrhack@gmail.com>
2021-02-16 15:34:14 +0300

    init

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function App() {
+  return (
+    <div className="h-screen flex justify-center items-center">
+      <p>hello from app</p>
+    </div>
+  );
+}
+
+export default App;
